3c283c264111dc58116d9cf35e931abfb925fee5
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2014-10-13 21:11:25 +0200

    Another way of starting everything. Perhaps I'll come back to this later, for now the other way keeps the extension and bookmarklet more similar.

--- src/start.js
+++ src/start.js
@@ -1,59 +1,31 @@
 (function() 
 {
     var x = "SubLimeStart_" + chrome.runtime.id;
+    var x2 = "SubLimeStart_RunAgain_" + chrome.runtime.id;
     var elem = document.getElementById(x);
     if (!elem)
     {
-        var data = [ "",
-    '            function mainInitialized()',
-    '            {',
-    '                console.log("Loaded main");',
-    '                ',
-    '                window.initializedSubOverlayBookmarklet = true;',
-    '                window.initializingSubOverlayBookmarklet = false;',
-    '                setTimeout(function() { subOverlayBookmarklet(); }, 0);',
-    '            }',
-    '',
-    '            function initBookmarklet()',
-    '            {',
-    '                var s = document.createElement("script");',
-    '                ',
-    '                s.src = "chrome-extension://' + chrome.runtime.id + '/sublimelet.js";',
-    '                s.onload = mainInitialized;',
-    '',
-    '                document.body.appendChild(s);',
-    '            }',
-    '',
-    '            function subOverlayBookmarklet()',
-    '            {',
-    '                if (window.initializingSubOverlayBookmarklet)',
-    '                    return;',
-    '',
-    '                if (!window.initializedSubOverlayBookmarklet)',
-    '                {',
-    '                    window.initializingSubOverlayBookmarklet = true;',
-    '                    initBookmarklet();',
-    '                    return;',
-    '                }',
-    '',
-    '                SubLimeLetRun("chrome-extension://' + chrome.runtime.id + '");',
-    '            }' ].join("\n");
-
-        data = "void((function() { " + data + "\nsubOverlayBookmarklet(); })())";
-
         elem = document.createElement("script");
         elem.setAttribute("id", x);
-        elem.innerHTML = data;
-        document.head.appendChild(elem);
+        elem.innerHTML = [ '(function()',
+                           '{',
+                           ' var s = document.createElement("script");',
+                           ' s.src = "chrome-extension://' + chrome.runtime.id + '/sublimelet.js";',
+                           ' s.onload = function() { SubLimeLetRun("chrome-extension://' + chrome.runtime.id + '"); }',
+                           ' document.body.appendChild(s);',
+                           '})()' ].join('\n');
+
+        document.body.appendChild(elem);
     }
     else
     {
-        var data = elem.innerHTML;
+        elem = document.getElementById(x2);
+        if (elem)
+            document.body.removeChild(elem);
 
-        document.head.removeChild(elem);
         elem = document.createElement("script");
-        elem.setAttribute("id", x);
-        elem.innerHTML = data;
-        document.head.appendChild(elem);
+        elem.setAttribute("id", x2);
+        elem.innerHTML = 'SubLimeLetRun("chrome-extension://' + chrome.runtime.id + '");';
+        document.body.appendChild(elem);
     }
 })();
