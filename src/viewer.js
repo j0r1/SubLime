418b65fd748d78841bc9439ae7f86738bd29e5bd
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2015-02-01 23:18:58 +0100

    Added some custom controls, from http://blog.teamtreehouse.com/building-custom-controls-for-html5-videos

--- src/viewer.js
+++ src/viewer.js
@@ -82,8 +82,6 @@ function fullscreen()
         elem.msRequestFullscreen();
     else if (elem.mozRequestFullScreen)
         elem.mozRequestFullScreen();
-    else if (elem.webkitRequestFullScreen)
-        elem.webkitRequestFullScreen();
     else if (elem.webkitRequestFullscreen)
         elem.webkitRequestFullscreen();
     else
@@ -128,6 +126,7 @@ function onMouseMove(evt)
 
     lastMouseTime = performance.now();
     $(videoDiv).css("cursor", "default");
+    $("#video-controls").show();
 }
 
 function onCheckVideoSizeTimeout()
@@ -136,22 +135,125 @@ function onCheckVideoSizeTimeout()
     var now = performance.now();
 
     if (now - lastMouseTime > 5000)
+    {
         $(videoDiv).css("cursor", "none");
-
+        $("#video-controls").hide();
+    }
     if (!videoElem)
         return;
 
     var w = video.videoWidth;
-    if (w <= 0)
-        return;
+    var h = video.videoHeight;
+    if (w <= 0 || h <= 0)
+    {
+        w = 16;
+        h = 9;
+    }
 
     var W = $(window).width();
-    var H = Math.round(W/w*video.videoHeight);
+    var H = Math.round(W/w*h);
+
+    var Hmax = $(window).height();
+    if (H > Hmax)
+    {
+        W = Math.round(H/h*w);
+        H = Hmax;
+    }
 
     videoElem.style.width = "" + W + "px";
     videoElem.style.height = "" + H + "px";
     videoDiv.style.width = "" + W + "px";
     videoDiv.style.height = "" + H + "px";
+}
+
+// Custom controls from http://blog.teamtreehouse.com/building-custom-controls-for-html5-videos
+function setupVideoControls()
+{
+	// Video
+	var video = document.getElementById("video");
+
+	// Buttons
+	var playButton = document.getElementById("play-pause");
+	var muteButton = document.getElementById("mute");
+	var fullScreenButton = document.getElementById("full-screen");
+
+	// Sliders
+	var seekBar = document.getElementById("seek-bar");
+	var volumeBar = document.getElementById("volume-bar");
+
+
+	// Event listener for the play/pause button
+	playButton.addEventListener("click", function() {
+		if (video.paused == true) {
+			// Play the video
+			video.play();
+
+			// Update the button text to 'Pause'
+			playButton.innerHTML = "Pause";
+		} else {
+			// Pause the video
+			video.pause();
+
+			// Update the button text to 'Play'
+			playButton.innerHTML = "Play";
+		}
+	});
+
+
+	// Event listener for the mute button
+	muteButton.addEventListener("click", function() {
+		if (video.muted == false) {
+			// Mute the video
+			video.muted = true;
+
+			// Update the button text
+			muteButton.innerHTML = "Unmute";
+		} else {
+			// Unmute the video
+			video.muted = false;
+
+			// Update the button text
+			muteButton.innerHTML = "Mute";
+		}
+	});
+
+	// Event listener for the full-screen button
+	fullScreenButton.addEventListener("click", fullscreen);
+
+	// Event listener for the seek bar
+	seekBar.addEventListener("change", function() {
+		// Calculate the new time
+		var time = video.duration * (seekBar.value / 100);
+
+		// Update the video time
+		video.currentTime = time;
+	});
+
+	
+	// Update the seek bar as the video plays
+	video.addEventListener("timeupdate", function() {
+		// Calculate the slider value
+		var value = (100 / video.duration) * video.currentTime;
+
+		// Update the slider value
+		seekBar.value = value;
+	});
+
+	// Pause the video when the seek handle is being dragged
+	seekBar.addEventListener("mousedown", function() {
+		video.pause();
+	});
+
+	// Play the video when the seek handle is dropped
+	seekBar.addEventListener("mouseup", function() {
+		video.play();
+	});
+
+	// Event listener for the volume bar
+	volumeBar.addEventListener("change", function() {
+		// Update the video volume
+		video.volume = volumeBar.value;
+	});
 }
 
 function onLoad()
@@ -178,9 +280,13 @@ function onLoad()
     videoElem = document.getElementById("video");
     videoDiv = document.getElementById("videodiv");
 
+    vex.defaultOptions.appendLocation = videoDiv;
+
     setInterval(onCheckVideoSizeTimeout, 1000);
 
 	window.addEventListener("mousemove", function(e) { onMouseMove(e); }, false);
+
+    setupVideoControls();
 }
 
 jQuery_2_1_0_for_vex(document).ready(onLoad);
